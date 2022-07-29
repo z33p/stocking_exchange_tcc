17d6281597b4b15d30a30d209dbb3a1029c5bf73
z33p <raphael00fellipe@gmail.com>
2022-07-29 12:31:08 -0300

    feat: dropdown with icon and better style

--- src/frontend/screens/TradeScreen/TradeScreen.tsx
+++ src/frontend/screens/TradeScreen/TradeScreen.tsx
@@ -86,7 +86,12 @@ function TradeScreen() {
             <div className="aux-button">
               <button className="btn-primary" disabled={!selectedTokenA} onClick={setFromToMaxValue}>Max</button>
               <div className="dropdown">
-                <span className="btn-gray">Tokens</span>
+                <p>
+                  <span style={{ paddingRight: .2 + "em" }}>Assets</span>
+                  <svg style={{ paddingBottom: .2 + "em" }}xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" width={1 + "em"}>
+                    <path d="M310.6 246.6l-127.1 128C176.4 380.9 168.2 384 160 384s-16.38-3.125-22.63-9.375l-127.1-128C.2244 237.5-2.516 223.7 2.438 211.8S19.07 192 32 192h255.1c12.94 0 24.62 7.781 29.58 19.75S319.8 237.5 310.6 246.6z" />
+                  </svg>
+                </p>
                 <div className="dropdown-content">
                   <ul>
                     {
